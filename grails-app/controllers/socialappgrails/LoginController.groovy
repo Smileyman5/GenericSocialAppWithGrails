4e3fb2b66bd02d4908595b4e6c55acf811ff9ee4
widdledude <cool.jonjon@gmail.com>
2017-04-19 17:54:45 -0400

    Git successful merge! :D

---
+++ grails-app/controllers/socialappgrails/LoginController.groovy
@@ -0,0 +1,45 @@
+package socialappgrails
+
+import grails.converters.JSON
+import org.grails.web.json.JSONObject
+
+/**
+ * Created by: Jonathan Baker
+ */
+
+class LoginController {
+
+    //accessed on a get request
+    //displays login page
+    def index() {
+        render (view: 'login.gsp')
+    }
+
+    //accessed on a post request
+    //accepts a username and password in JSON
+    //in JSON, returns login failed if result['failed'] = true
+    //in JSON, returns login succeeds if result['loggedin'] = true
+    def login() {
+        JSONObject jsonObject = request.JSON
+        String username = jsonObject.get("username")
+        String password = jsonObject.get("password")
+
+        def result = [:]
+        result['failed'] = false
+        result['loggedin'] = false
+
+        if (checkLogin(username, password)) {
+            result['loggedin'] = true
+        }
+        else {
+            result['failed'] = true
+        }
+
+        render result as JSON
+    }
+
+    def checkLogin(String username, String password) {
+        return false
+    }
+
+}
